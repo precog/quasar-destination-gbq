b152f844cab3a9471827529ef72e1161e17fdc23
nicflores <nic@slamdata.com>
2020-01-24 10:15:52 -0700

    add debug statements in decrypt script

--- scripts/decrypt_gbq_auth.sh
+++ scripts/decrypt_gbq_auth.sh
@@ -7,8 +7,18 @@ PRIVATE_KEY=${TRAVIS_BUILD_DIR}/scripts/gbq-enc-private.key
 ENC_KEY_FILE=${TRAVIS_BUILD_DIR}/scripts/keyfile.enc
 DEC_KEY_FILE=${TRAVIS_BUILD_DIR}/scripts/keyfile.txt
 ENC_AUTH_FILE=${TRAVIS_BUILD_DIR}/scripts/gbqAuthFile.enc
-DEC_AUTH_FILE=$TRAVIS_BUILD_DIR/core/src/test/resources/gbqAuthFile.json
+DEC_AUTH_FILE=${TRAVIS_BUILD_DIR}/core/src/test/resources/gbqAuthFile.json
+
+ls -al ${TRAVIS_BUILD_DIR}/scripts
+echo $PRIVATE_KEY
+echo $ENC_KEY_FILE
+echo $DEC_KEY_FILE
+echo $ENC_AUTH_FILE
+echo $DEC_AUTH_FILE
 
 openssl rsautl -decrypt -inkey ${PRIVATE_KEY} -in ${ENC_KEY_FILE} -out ${DEC_KEY_FILE} -passin pass:${GBQ_DECRYPT_PASSWORD}
+
+echo "decrypted $DEC_KEY_FILE"
+ls -al ${TTRAVIS_BUILD_DIR}/scripts
 
 openssl enc -d -aes-256-cbc -in ${ENC_AUTH_FILE} -out ${DEC_AUTH_FILE} -pass file:${DEC_KEY_FILE}
