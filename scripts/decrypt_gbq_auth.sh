804d48c0d5f3d43891c94c99d1c5bdfbe46e1813
nicflores <nic@slamdata.com>
2020-01-24 11:20:29 -0700

    debug trace statements in tavis file

--- scripts/decrypt_gbq_auth.sh
+++ scripts/decrypt_gbq_auth.sh
@@ -9,18 +9,6 @@ DEC_KEY_FILE=${TRAVIS_BUILD_DIR}/scripts/keyfile.txt
 ENC_AUTH_FILE=${TRAVIS_BUILD_DIR}/scripts/gbqAuthFile.enc
 DEC_AUTH_FILE=${TRAVIS_BUILD_DIR}/scripts/gbqAuthFile.json
 
-ls -al ${TRAVIS_BUILD_DIR}/scripts
-echo $PRIVATE_KEY
-echo $ENC_KEY_FILE
-echo $DEC_KEY_FILE
-echo $ENC_AUTH_FILE
-echo $DEC_AUTH_FILE
-
 openssl rsautl -decrypt -inkey ${PRIVATE_KEY} -in ${ENC_KEY_FILE} -out ${DEC_KEY_FILE} -passin pass:${GBQ_DECRYPT_PASSWORD}
 
-echo "decrypted $DEC_KEY_FILE"
-ls -al ${TRAVIS_BUILD_DIR}/scripts
-
 openssl enc -d -aes-256-cbc -in ${ENC_AUTH_FILE} -out ${DEC_AUTH_FILE} -pass file:${DEC_KEY_FILE}
-
-echo "decrypted gbq auth json file"
